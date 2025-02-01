79e4e3755a2fbb97183a43728734f0cdaa1899e7
dallas <dallas@A6-MBA-DALLAS.local>
2025-01-31 23:41:16 -0700

    add wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Snazzy'
+
+config.font_size = 14
+
+config.window_decorations = 'TITLE|RESIZE'
+config.enable_tab_bar = false
+return config
