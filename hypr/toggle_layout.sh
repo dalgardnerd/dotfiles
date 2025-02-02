411aa8c43f6267ee147a057a7caf7dc564141047
Dallas Gardner <dalgardnerd@gmail.com>
2025-02-01 23:22:41 -0700

    remove toggle_layout.sh from wrong folder

--- hypr/toggle_layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current_mode=$(hyprctl getoption general:layout | grep str | awk '{print $2}')
-if [ ${current_mode} == 'master' ]; then
-  hyprctl keyword general:layout dwindle
-else
-  hyprctl keyword general:layout master
-fi
