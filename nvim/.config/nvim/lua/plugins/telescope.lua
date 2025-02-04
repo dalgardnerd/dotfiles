31eeeffb9e2461ae74935550e957a3f33d6c462c
Dallas Gardner <dalgardnerd@gmail.com>
2025-02-04 15:39:38 -0700

    update telescope config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,14 +4,22 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    tag = "0.1.8",
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
       require("telescope").setup()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<space>fd", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
 
       require("telescope").load_extension("ui-select")
     end,
